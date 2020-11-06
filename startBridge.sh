eab15a8a1eed99be245bde2642e05f3002491883
davewins <davidwinstone@gmail.com>
2020-11-06 14:20:22 +0000

    Updated start script with region/source

--- startBridge.sh
+++ startBridge.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /home/pi/dump1090toTIBCOStreamingBridge
-./dump1090toTIBCOStreamingBridge --dump1090URL localhost:30003 -streamingHostURL <URL> -streamingHostUsername <USERNAME> --streamingHostPassword <PASSWORD> 2>&1 > /var/log/dump1090toTIBCO.log
+./dump1090toTIBCOStreamingBridge --dump1090URL localhost:30003 -streamingHostURL <URL> -streamingHostUsername <USERNAME> --streamingHostPassword <PASSWORD> --region UK --sourceID JohnSmith 2>&1 > /var/log/dump1090toTIBCO.log
